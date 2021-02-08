8b4504715647f871f1ba42f8e3b18d3ddf3bfce0
David Bruck <dbruck1@fau.edu>
2021-02-07 22:47:28 -0500

    https://www.pivotaltracker.com/story/show/176838435

---
+++ svelte-app/src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Footer from './components/Footer.svelte'
+</script>
+
+<h1>CEN5035 Spring 2021 David Bruck Project</h1>
+<Footer></Footer>
+
+<style>
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
