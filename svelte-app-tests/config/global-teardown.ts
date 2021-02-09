46fb320e7b674b11fa6ad3ddf094a52d43d82701
David Bruck <dbruck1@fau.edu>
2021-02-09 08:21:59 -0500

    Formatting

--- svelte-app-tests/config/global-teardown.ts
+++ svelte-app-tests/config/global-teardown.ts
@@ -1,4 +1,4 @@
-import { teardown as teardownDevServer } from 'jest-dev-server'
+import { teardown as teardownDevServer } from 'jest-dev-server';
 
 const { teardown: teardownPuppeteer }: { teardown: (_: any) => Promise<void> } =
     require('jest-environment-puppeteer');
