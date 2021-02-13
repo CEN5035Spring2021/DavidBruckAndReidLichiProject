e6dc1b813301aaecbf1cd942aed66cc957413ffc
David Bruck <dbruck1@fau.edu>
2021-02-12 20:17:20 -0500

    User creation with local public/private key pair
    https://www.pivotaltracker.com/story/show/176838445

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,27 +1,35 @@
+<script context=module>
+    // Must import in reverse dependency order so bundle.js loads regeneratorRuntime before shimIndexedDB
+    import './modules/indexedDB';
+    import '../node_modules/indexeddbshim/dist/indexeddbshim';
+    import '../node_modules/regenerator-runtime/runtime';
+    import '../node_modules/core-js-bundle/minified';
+</script>
+
 <script lang=ts>
-	import Footer from './components/Footer.svelte'
-	import Login from './components/Login.svelte'
-	import { privateKey } from './stores/user'
+    import Footer from './components/Footer.svelte';
+    import Login from './components/Login.svelte';
+    import { privateKey } from './stores/user';
 </script>
 
 <h1>
-	Secure Group Messenger
-	<br />
-	<small>CEN5035 Spring 2021 David Bruck Project</small>
+    Secure Group Messenger
+    <br />
+    <small>CEN5035 Spring 2021 David Bruck Project</small>
 </h1>
 { #if ($privateKey) }
-	<b>Logged in!</b>
+    <b>Logged in!</b>
 { :else }
-	<Login />
+    <Login />
 { /if }
 <br />
 <Footer />
 
 <style>
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 2em;
-		font-weight: 100;
-	}
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 2em;
+        font-weight: 100;
+    }
 </style>
