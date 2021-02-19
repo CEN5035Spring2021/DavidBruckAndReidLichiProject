53918b6fdbd4fc67a0346a37b609674528b917bd
David Bruck <dbruck1@fau.edu>
2021-02-19 01:01:07 -0500

    Add Reid Lichi to the group (add his name to authors)
    https://www.pivotaltracker.com/story/show/177011989

---
+++ svelte-app/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    root: true,
+    extends: [
+        '../.eslintrc.js'
+    ],
+    parserOptions: {
+        project: [
+            './svelte-app/tsconfig.json'
+        ],
+        extraFileExtensions: [
+            '.svelte'
+        ]
+    },
+    rules: {
+        'no-magic-numbers': [
+            'error',
+            {
+                ignore: [
+                    1
+                ],
+                detectObjects: true
+            }
+        ],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'off'
+    }
+};
---
+++ svelte-app/src/types/dbTransaction.d.ts
@@ -0,0 +1,3 @@
+declare interface IDBTransaction {
+    commit?: () => void;
+}
