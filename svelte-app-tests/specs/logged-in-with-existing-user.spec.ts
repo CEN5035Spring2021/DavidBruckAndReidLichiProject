6e59973e95b1218ee97527a85e7503c6a09f9c92
David Bruck <dbruck1@fau.edu>
2021-02-20 15:08:25 -0500

    Swapped tests for creating user versus existing user login; fixed IDE warnings
    IDE warnings fixed:
    ```
    Parsing error: "parserOptions.project" has been set for @typescript-eslint/parser.
    ```
    
    https://www.pivotaltracker.com/story/show/177011989

--- svelte-app-tests/specs/logged-in-with-existing-user.spec.ts
+++ svelte-app-tests/specs/logged-in-with-existing-user.spec.ts
@@ -1,29 +1,47 @@
 import 'expect-puppeteer';
 
-describe('Created user is logged-in', () => {
+describe('Login with existing user', () => {
     beforeAll(async() => {
         await page.goto('http://127.0.0.1:5000');
         const createNewUser = await page.$('button');
 
         await createNewUser?.click();
 
-        const emailInput = await page.$('[ id="newEmail" ]');
-        const passwordInput = await page.$('[ id="newPassword" ]');
+        const newEmailInput = await page.$('[ id="newEmail" ]');
+        const newPasswordInput = await page.$('[ id="newPassword" ]');
         const confirmPasswordInput = await page.$('[ id="confirmPassword" ]');
-        const password = 'MyT3stP@ss';
-
-        await emailInput?.type('logged-in-with-existing-user@cen.5035');
-        await passwordInput?.type(password);
-        await confirmPasswordInput?.type(password);
-    });
-
-    it('Created user is logged-in', async () => {
         const createUserButtonSelector = '.modal > div > input[ type="button" ]';
         const createUserButton = await page.$(createUserButtonSelector);
+        const emailAddress = 'created-user-is-logged-in@cen.5035';
+        const password = 'MyT3stP@ss';
+
+        await newEmailInput?.type(emailAddress);
+        await newPasswordInput?.type(password);
+        await confirmPasswordInput?.type(password);
 
         await createUserButton?.click();
         await page.waitForSelector(
             createUserButtonSelector,
+            {
+                hidden: true,
+                timeout: 30000
+            });
+        await page.reload();
+
+        const emailInput = await page.$('[ id="email" ]');
+        const passwordInput = await page.$('[ id="password" ]');
+
+        await emailInput?.type(emailAddress);
+        await passwordInput?.type(password);
+    });
+
+    it('Existing user can login', async () => {
+        const loginButtonSelector = 'fieldset > input[ type="button" ]';
+        const loginButton = await page.$(loginButtonSelector);
+
+        await loginButton?.click();
+        await page.waitForSelector(
+            loginButtonSelector,
             {
                 hidden: true,
                 timeout: 30000
